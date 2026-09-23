e2ced2df5695c5967106ece3933129f948ef0b92
EmmaRenauld <emmanuelle.renauld@usherbrooke.ca>
2026-09-23 11:20:17 -0400

    Rochester

---
+++ 0_bids/convert_Rochester.sh
@@ -0,0 +1,72 @@
+# --------------------------------------
+# Script to convert raw data into BIDS organization
+# Creates links as relative paths. Else, it has my username
+# in the link and it becomes broken for other users.
+#
+# Inputs:
+#   Source: Rochester
+#     Two sub-folders: CBP and Healthy. But not the same numbers
+#     Names are, for instance: sub-1009
+#
+# Output: bids/sub-Roch*
+#
+# author: Emmanuelle Renauld
+# 2026
+# ---------------------------------------
+
+bids="$HOME/projects/def-pascalt-ab/ProjectCLBP_multisite/bids"
+
+main_folder=CLBP_Databases/Rochester
+source_path="$HOME/projects/def-pascalt-ab/$main_folder"
+
+# One .. to get out of bids, another to get out of our Project.
+relative_path="../../$main_folder"
+
+echo "Looking for subjects in : $source_path"
+
+for subj_folder in $source_path/*/*
+do
+
+    # --- Extracting subject name
+    old_name=${subj_folder#$source_path/}
+    subfolder=${old_name%%/*}
+    old_name=${old_name#CBP/}
+    old_name=${old_name#Healthy/}
+
+    # --- Extracting number only
+    number=${old_name#sub-}
+
+    # --- New name
+    new_name=sub-Roch$number
+    echo "$old_name ----> $new_name"
+
+    # --- Folder
+    if [ -d $bids/$new_name ]
+    then
+        echo "WARNING. SUBJECT EXISTED! DELETE FIRST"
+        continue
+    else
+        mkdir $bids/$new_name
+    fi
+
+    # --- Anat
+    # Relative: Two more .. for the relative path to get out of anat, and then subject.
+    old_anat_name=anat/${old_name}_ses-bsl_T1w_defaced.nii.gz
+    old_anat=$source_path/$subfolder/$old_name/$old_anat_name
+    old_anat_relative=../../$relative_path/$subfolder/$old_name/$old_anat_name
+
+    new_anat=$bids/$new_name/anat/${new_name}_T1w.nii.gz
+
+    if [ ! -f $old_anat ]
+    then
+        echo "... anat not found. Skipped. Where is $old_anat?"
+    else
+        mkdir $bids/$new_name/anat
+        ln -s $old_anat_relative $new_anat
+    fi
+
+done
+
+
+echo "Done. Please verify the symlinks: tree $bids/sub-Roch*"
+
